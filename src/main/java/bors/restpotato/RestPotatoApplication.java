a692db4b6dd8455c1eec6b40b4a4e97498d960bd
Boris Ivanov <bors2908@gmail.com>
2020-07-21 21:31:12 +0300

    Spring Initializr

---
+++ src/main/java/bors/restpotato/RestPotatoApplication.java
@@ -0,0 +1,13 @@
+package bors.restpotato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPotatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestPotatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bors/restpotato/RestPotatoApplicationTests.java
@@ -0,0 +1,13 @@
+package bors.restpotato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestPotatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
